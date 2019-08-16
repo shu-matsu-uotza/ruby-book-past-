a99d5f17d38a94030fe5d1b71ebf1425ef9a8f64
shu-matsu-uotza <sakana.color26@gmail.com>
2019-08-17 08:09:58 +0900

    fix
    Fizz Buzzの記述ミスを修正

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -10,6 +10,6 @@ class FizzBuzzTest < Minitest::Test
 		assert_equal 'Buzz', fizz_buzz(5)
 		assert_equal 'Fizz', fizz_buzz(6)
 		assert_equal '7', fizz_buzz(7)
-		assert_equal 'FizzBuzz', fizz_buzz(15)
+		assert_equal 'Fizz Buzz', fizz_buzz(15)
 	end
 end
